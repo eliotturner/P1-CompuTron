5f6212632b3c01549b6b270ecb817ba36a5cc3b8
eliotturner <eliotturner@vt.edu>
2025-02-28 22:32:48 -0500

    add final comments and some formatting changes

--- include/computron.h
+++ include/computron.h
@@ -17,20 +17,25 @@ enum class Command {
 	branch = 40, branchNeg, branchZero, halt
 };
 
+//Loads file into memory word by word
 void load_from_file(std::array<int, memorySize>& memory, const std::string& filename);
 
+//executes program loaded into memory
 void execute(std::array<int, memorySize>& memory, int* const acPtr,
 	size_t* const icPtr, int* const irPtr,
 	size_t* const opCodePtr, size_t* const opPtr,
 	const std::vector<int>& inputs
 	);
 
+//dump all memory data and register contents into console
 void dump(std::array<int, memorySize>& memory, int* const acPtr,
 	size_t instructionCounter, size_t instructionRegister,
 	size_t operationCode, size_t operand);
 
+//check valid word
 bool validWord(int word);
 
+//helper function for dump, prints output
 void output(std::string label, int width, int value, bool sign);
 
 #endif
--- src/computron.cpp
+++ src/computron.cpp
@@ -5,23 +5,28 @@
 
 void load_from_file(std::array<int, memorySize>& memory, const std::string& filename)
 {
+	//set vars
 	constexpr int sentinel{ -99999 };
 	size_t i{ 0 };
 	std::string line;
 	int instruction;
 
+	//open file and check open
 	std::ifstream inputFile(filename);
 	if (!inputFile)
 	{
 		throw std::runtime_error("invalid_input");
 	}
 
+	//get each line
 	while (std::getline(inputFile, line))
 	{
+		//extract instruction and check for sentinel
 		instruction = std::stoi(line);
 		if (instruction == sentinel)
 			break;
 
+		//add valid words to memory, exit if invalid word
 		if (validWord(instruction))
 		{
 			memory[i] = instruction;
@@ -36,6 +41,7 @@ void load_from_file(std::array<int, memorySize>& memory, const std::string& file
 
 Command opCodeToCommand(size_t opCode)
 {
+	//return relevant enum element for each opcode
 	switch (opCode)
 	{
 		case 10: return Command::read; break;
@@ -63,34 +69,36 @@ void execute(std::array<int, memorySize>& memory, int* const acPtr,
 
 	do
 	{
+		//extract full instruction, opcode, and operand
 		*irPtr = memory[*icPtr];
 		*opCodePtr = *irPtr / 100;
 		*opPtr = *irPtr % 100;
 
+		//switch based on opcode
 		switch (int word{}; opCodeToCommand(*opCodePtr))
 		{
 			case Command::read:
-				word = inputs[inputIndex];
-				memory[*opPtr] = word;
-				++(*icPtr);
+				word = inputs[inputIndex]; //read input
+				memory[*opPtr] = word; //write to mem
+				++(*icPtr); 
 				inputIndex++;
 				break;
 			case Command::write:
-				++(*icPtr);
+				++(*icPtr); 
 				//std::cout << "Contents of " << std::setfill('0') << std::setw(4);
 				//std::cout << *opPtr << " : " << memory[*opPtr] << "\n";
 				break;
 			case Command::load:
-				*acPtr = memory[*opPtr];
+				*acPtr = memory[*opPtr]; //write mem to acc
 				++(*icPtr);
 				break;
 			case Command::store:
-				memory[*opPtr] = *acPtr;
+				memory[*opPtr] = *acPtr; //write acc to mem
 				++(*icPtr);
 				break;
 			case Command::add:
-				word = *acPtr + memory[*opPtr];
-				if (validWord(word))
+				word = *acPtr + memory[*opPtr]; //do operation
+				if (validWord(word)) //check valid & write acc
 				{
 					*acPtr = word;
 					++(*icPtr);
@@ -99,8 +107,8 @@ void execute(std::array<int, memorySize>& memory, int* const acPtr,
 					throw std::runtime_error("invalid_input");
 				break;
 			case Command::subtract:
-				word = *acPtr - memory[*opPtr];
-				if (validWord(word))
+				word = *acPtr - memory[*opPtr]; //do operation
+				if (validWord(word)) //check valid & write acc
 				{
 					*acPtr = word;
 					++(*icPtr);
@@ -109,8 +117,8 @@ void execute(std::array<int, memorySize>& memory, int* const acPtr,
 					throw std::runtime_error("invalid_input");
 				break;
 			case Command::multiply:
-				word = *acPtr * memory[*opPtr];
-				if (validWord(word))
+				word = *acPtr * memory[*opPtr]; //do operation
+				if (validWord(word)) //check valid & write acc
 				{
 					*acPtr = word;
 					++(*icPtr);
@@ -119,10 +127,10 @@ void execute(std::array<int, memorySize>& memory, int* const acPtr,
 					throw std::runtime_error("invalid_input");
 				break;
 			case Command::divide:
-				if (memory[*opPtr] == 0)
+				if (memory[*opPtr] == 0) //div-by-zero check
 					throw std::runtime_error("invalid_input");
-				word = *acPtr / memory[*opPtr];
-				if (validWord(word))
+				word = *acPtr / memory[*opPtr]; //do operation
+				if (validWord(word)) //check valid & write acc
 				{
 					*acPtr = word;
 					++(*icPtr);
@@ -131,13 +139,13 @@ void execute(std::array<int, memorySize>& memory, int* const acPtr,
 					throw std::runtime_error("invalid_input");
 				break;
 			case Command::branch:
-				*icPtr = *opPtr;
+				*icPtr = *opPtr; //update instruction counter
 				break;
 			case Command::branchNeg:
-				*acPtr < 0 ? *icPtr = *opPtr : ++(*icPtr);
+				*acPtr < 0 ? *icPtr = *opPtr : ++(*icPtr); //check negative, then branch
 				break;
 			case Command::branchZero:
-				*acPtr == 0 ? *icPtr = *opPtr : ++(*icPtr);
+				*acPtr == 0 ? *icPtr = *opPtr : ++(*icPtr); //check zero, then branch
 				break;
 			case Command::halt:
 				//dump(memory, acPtr, *icPtr, *irPtr, *opCodePtr, *opPtr);
@@ -154,12 +162,15 @@ void dump(std::array<int, memorySize>& memory, int* const acPtr,
 	size_t instructionCounter, size_t instructionRegister,
 	size_t operationCode, size_t operand)
 {
+	//print top-label
 	std::cout << "Memory:" << "\n";
+
 	//print column labels
 	std::cout << "    "; //pad
 	for (int col = 0; col < 10; col++)
 		std::cout << col << "    ";
 	std::cout << "\n";
+
 
 	for (int row = 0; row < 10; ++row) {
 		//print row label
@@ -168,6 +179,7 @@ void dump(std::array<int, memorySize>& memory, int* const acPtr,
 		// print memory in row
 		for (int col = 0; col < 10; ++col)
 		{
+			//check sign and print accordingly
 			int val = memory[row * 10 + col];
 			if (val < 0)
 				std::cout << "-" << std::setw(4) << std::setfill('0') << abs(memory[row * 10 + col]);
@@ -177,11 +189,20 @@ void dump(std::array<int, memorySize>& memory, int* const acPtr,
 
 		std::cout << "\n";
 	}
+
+	//print register contents using output function
+	std::cout << '\n' << "Registers" << '\n';
+	output("Accumulator", 4, *acPtr, true);
+	output("instructionCounter", 2, instructionCounter, false);
+	output("instructionRegister", 4, instructionRegister, true);
+	output("operationCode", 2, operationCode, false);
+	output("operand", 2, operand, false);
+	std::cout << '\n';
 }
 
 bool validWord(int word)
 {
-	//check bounds
+	//check for outside word bounds
 	if (word > maxWord || word < minWord)
 		return false;
 
@@ -190,9 +211,21 @@ bool validWord(int word)
 
 void output(std::string label, int width, int value, bool sign)
 {
-	std::cout << label << "\t\t";
+	//print the label with a constant width for formatting
+	std::cout << std::setw(22) << std::setfill(' ') << std::left << label << "\t";
+	
+	//add sign if necessary
 	if (sign)
-		std::cout << '+' << std::setw(width) << value;
-	else
-		std::cout << '-' << std::setw(width) << value;
+	{
+		if (value < 0)
+			std::cout << "-";
+		else
+			std::cout << "+";
+	}
+
+	//add zero-filled value (signless)
+	std::cout << std::setfill('0') << std::setw(width) << abs(value) << '\n';
+
+	//reset cout formatting
+	std::cout << std::right;
 }
